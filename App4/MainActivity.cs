e0ab97c41e9d5321ca52c737f0c9df5c84b966ea
zhakhov <zhakhov@smt-samara.ru>
2016-06-03 11:30:14 +0400

    te

--- App4/MainActivity.cs
+++ App4/MainActivity.cs
@@ -17,6 +17,7 @@ namespace App4
         float count = 0;
         float countnul = 1;
         Button button;
+        int a = 0;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
